a2deb674f99a89b7518ce98e5f66b87543c9b46e
Lorenzo Martegiani <lorenzomartegiani@gmail.com>
2022-12-14 17:20:13 +0100

    piano piano finisco java..

--- D5.js
+++ D5.js
@@ -11,17 +11,21 @@ REGOLE
 */
 const pets = ['dog', 'cat', 'hamster', 'redfish']
 
-/* ESERCIZIO 2
-    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
-*/
+// ESERCIZIO 2
+ //   Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+pets.sort;
+console.log(pets)
 
-/* ESERCIZIO 3
-    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
-*/
+// ESERCIZIO 3
+ //   Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+let pipo = pets.reverse();
+console.log(pipo)
 
-/* ESERCIZIO 4
-    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
-*/
+// ESERCIZIO 4
+  //  Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+pets.shift(4);
+pets.push("redfish")
+console.log(pets)
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -46,6 +50,12 @@ const cars = [
     trims: ['life', 'style', 'r-line'],
   },
 ]
+for (let i = 0; i < cars.length; i++){
+  (cars[0].licensePlate= "Numero1"),
+  (cars[1].licensePlate= "Numero2"),
+  (cars[2].licensePlate= "Numero3"),
+  console.log(cars)
+}
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
